3ebece59acec6884aa884d707073098355cb8a8d
Nicolas Herment <nherment@arkhaios.net>
2014-02-02 08:43:46 -0800

    delete useless code

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -18,21 +18,3 @@ phonecatControllers.controller('SignUpCtrl', ['$scope', 'backend',
   function($scope, backend) {
 //    backend.send('hello')
   }]);
-
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', 'backend',
-  function($scope, Phone, backend) {
-    backend.send('hello')
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
